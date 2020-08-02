be5de70d8e9b22012ba5f24604f550c73534573e
jamesnascimento1994 <james.nascimento94@yahoo.com>
2020-08-02 18:14:58 -0400

    added personnel to albums

--- src/AlbumDisplay.js
+++ src/AlbumDisplay.js
@@ -16,6 +16,12 @@ class AlbumDisplay extends Component {
 				<img src={albumToDisplay.artworkUrl100} alt='artwork' />
 				<h2>{albumToDisplay.collectionName}</h2>
 				<h3>{albumToDisplay.releaseDate}</h3>
+				<h4>Personnel:</h4>
+				<ul>
+					<li>{albumToDisplay.personnel[0]}</li>
+					<li>{albumToDisplay.personnel[1]}</li>
+					<li>{albumToDisplay.personnel[2]}</li>
+				</ul>
 			</div>
 		);
 	}
